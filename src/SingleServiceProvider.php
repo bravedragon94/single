ccb52a203d114079f0bdd0bd7da3ff4074c57fd5
bravedragon <bravedragon94@mail.ru>
2020-06-18 17:10:50 +1000

    create

---
+++ src/SingleServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace jboss\Single;
+
+use Illuminate\Support\ServiceProvider;
+
+class SingleServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
+    }
+}
---
+++ src/route.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JBOSS
+ * Date: 6/17/2020
+ * Time: 6:14 PM
+ */
+    use Illuminate\Support\Facades\Route;
+
+    Route::get('single', function(){
+        echo 'Hello from the calculator package!';
+    });
